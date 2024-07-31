56209cd6417e1d120e3493fb6be49454b543fccc
wesley <543118962@qq.com>
2024-07-31 17:18:12 +0800

    change feature

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import {Calendar, Field, Form} from 'vant'
+import vant from 'vant'
 import 'vant/lib/index.css'
 
 const app = createApp(App)
 
-app.use(Form)
-app.use(Field)
-app.use(Calendar)
+app.use(vant)
 app.mount('#app')
